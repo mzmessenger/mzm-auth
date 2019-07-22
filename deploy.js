29d8879c54bf8e803a80ec1125001a1c069246b7
kohta ito <kohta110@gmail.com>
2019-07-23 00:00:49 +0900

    add deploy.js

---
+++ deploy.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+/**
+ * ./deploy.js -h host -u username
+ */
+
+/* eslint-disable no-console */
+
+const path = require('path')
+const rmtcmd = require('rmtcmd')
+
+async function deploy({ config, remote, local }) {
+  await local('npm test', { cwd: __dirname })
+
+  const target = '/var/www/mzm-auth'
+  const tmpDir = '/tmp/mzm-auth'
+  const src = path.join(__dirname, 'dist')
+
+  await remote(`sudo mkdir -p ${target}`)
+  await remote(`sudo mkdir -p ${tmpDir}`)
+  await remote(`sudo chown -R ${config.username} ${tmpDir}`)
+
+  await local('npm run build', { cwd: __dirname })
+  await local(`cp package.json ${src}`, { cwd: __dirname })
+  await local(`cp package-lock.json ${src}`, { cwd: __dirname })
+
+  await local(
+    [
+      `rsync -av`,
+      `--exclude='node_modules'`,
+      `--exclude='.env'`,
+      `-e 'ssh -i ${config.privateKeyPath}'`,
+      `${src}/`,
+      `${config.username}@${config.host}:${tmpDir}/`
+    ].join(' '),
+    {
+      cwd: __dirname
+    }
+  )
+
+  await remote(`cd ${tmpDir} && sudo npm install --production`)
+  await remote(`sudo rsync -av --exclude='.env' ${tmpDir}/ ${target}/`)
+  await remote(`sudo systemctl restart mzm-auth`)
+}
+
+;(async () => {
+  const args = await rmtcmd.cli.getArgs()
+  await rmtcmd.connect({ ...args, task: deploy })
+})().catch(console.error)
